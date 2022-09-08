98960770dc26350b53c47ceb6cb4e987b93b09d1
Arturo Suárez <manuel.suarez@cimat.mx>
2022-09-08 18:05:16 -0500

    Definition of training step

--- main.py
+++ main.py
@@ -348,3 +348,45 @@ for xl_input, xr_input, y_input in train_xy.take(1):
     generate_images('figure_4.png', generator, xl_input, xr_input, y_input)
     print(xl_input.shape, xr_input.shape, y_input.shape)
     break
+
+log_dir="logs/"
+
+summary_writer = tf.summary.create_file_writer(
+  log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+# Training step
+@tf.function
+def train_step(left_image, right_image, target, step):
+    '''
+    Cálculos realizados durante un paso del entrenamiento
+
+    Dadas los pares x,y (suavizada, real):
+    - Genera datos sintéticos x' con Unet
+    - Evalua el discriminador para los pares suavizado-(x,y) y texturizado-(x',y)
+    - Evalua los costos del generador y del discriminador
+    - Calcula los gradiente
+    - Realiza los pasos de optimización
+    - Reporta loss y métricas
+    '''
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        gen_output = generator([left_image, right_image], training=True)
+
+        disc_real_output = discriminator([left_image, right_image, target], training=True)
+        disc_generated_output = discriminator([left_image, right_image, gen_output], training=True)
+
+        gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(disc_generated_output, gen_output, target)
+        disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
+
+    generator_gradients = gen_tape.gradient(gen_total_loss, generator.trainable_variables)
+    discriminator_gradients = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(generator_gradients, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(discriminator_gradients, discriminator.trainable_variables))
+
+    with summary_writer.as_default():
+        ss = step // 1000
+        tf.summary.scalar('gen_total_loss', gen_total_loss, step=ss)
+        tf.summary.scalar('gen_gan_loss', gen_gan_loss, step=ss)
+        tf.summary.scalar('gen_l1_loss', gen_l1_loss, step=ss)
+        tf.summary.scalar('disc_loss', disc_loss, step=ss)
