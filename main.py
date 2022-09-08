6baa25027961b4cfcb536615825943a680617a5a
Arturo Suárez <manuel.suarez@cimat.mx>
2022-09-08 17:48:12 -0500

    Initial setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import pathlib
+import time
+import datetime
+from IPython import display
+
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+print(tf.__version__)
