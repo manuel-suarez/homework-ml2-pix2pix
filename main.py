ed295e72fb5b91ac8711ac8d0ee533b12764a5b9
Arturo Suárez <manuel.suarez@cimat.mx>
2022-09-08 17:53:00 -0500

    Configure path of files

--- main.py
+++ main.py
@@ -12,3 +12,29 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 print(tf.__version__)
+
+# Dimensions definition
+INPUT_DIM     = (256,256,3)
+OUTPUT_CHANNELS = 1 #INPUT_DIM[-1]
+BATCH_SIZE    = 10
+R_LOSS_FACTOR = 10000
+EPOCHS        = 100
+INITIAL_EPOCH = 0
+
+# Define local dir for images
+data_dir = '/home/est_posgrado_manuel.suarez/data/sintel'
+# Define expresions to load images
+xl_files = glob(os.path.join(data_dir, 'final_left', '**/*.png'))
+xr_files = glob(os.path.join(data_dir, 'final_right', '**/*.png'))
+y_files  = glob(os.path.join(data_dir, 'disparities_viz', '**/*.png'))
+# Sort
+xl_files.sort()
+xr_files.sort()
+y_files.sort()
+# Convert to np arrays
+xl_files = np.array(xl_files)
+xr_files = np.array(xr_files)
+y_files  = np.array(y_files)
+# Check first 5 files
+for xl, xr, y in zip(xl_files[:5], xr_files[:5], y_files[:5]):
+  print(xl, xr, y)
